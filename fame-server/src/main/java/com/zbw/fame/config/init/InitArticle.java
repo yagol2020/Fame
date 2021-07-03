e7c0b2f6978c425051d7e2a0be5bef47ab4503e8
yagol2020 <67462255+yagol2020@users.noreply.github.com>
2021-07-03 10:26:42 +0800

    修改：博客初始化添加，根据文件夹区分类别

--- fame-server/src/main/java/com/zbw/fame/config/init/InitArticle.java
+++ fame-server/src/main/java/com/zbw/fame/config/init/InitArticle.java
@@ -29,29 +29,22 @@ public class InitArticle {
 
     final YagolProperties.Init yagolInit;
 
-    final static String SPLIT_CHAR = ":";
-
     public List<Article> initArticle(User user) throws IOException {
         File articleDir = new File(Objects.requireNonNull(getClass().getResource("/")).getPath()
                 + yagolInit.getContentPath());
         List<Article> articles = new LinkedList<>();
-        for (File articleFile : Objects.requireNonNull(articleDir.listFiles())) {
-            Article article = new Article();
-            String articleFileName = articleFile.getName();
-            try {
-                article.setCategoryTemp(articleFileName.split(SPLIT_CHAR)[0]);
-                article.setTitle(articleFileName.split(SPLIT_CHAR)[1].replace(".md", ""));
-            } catch (Exception exception) {
-                log.error("初始化博客时出现错误！查看是否存在\":\"号");
-                article.setCategoryTemp("Error");
-                article.setTitle("Error");
+        for (File dirFile : Objects.requireNonNull(articleDir.listFiles())) {
+            for (File articleFile : Objects.requireNonNull(dirFile.listFiles())) {
+                Article article = new Article();
+                article.setCategoryTemp(dirFile.getName());
+                article.setTitle(articleFile.getName().replace(".md", StringUtils.EMPTY));
+                article.setContent(YagolUtils.readFile2Str(articleFile));
+                article.setHeaderShow(false);
+                article.setListShow(true);
+                article.setStatus(ArticleStatus.PUBLISH);
+                article.setAuthorId(user.getId());
+                articles.add(article);
             }
-            article.setContent(YagolUtils.readFile2Str(articleFile));
-            article.setHeaderShow(false);
-            article.setListShow(true);
-            article.setStatus(ArticleStatus.PUBLISH);
-            article.setAuthorId(user.getId());
-            articles.add(article);
         }
         return articles;
     }
